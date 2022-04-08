39a29985109b34621891d2d3e5b5deba5e6d9de9
Ombre <krys1014@wp.pl>
2022-04-08 20:11:29 +0200

    Add file-loader to next.config.js

--- next.config.js
+++ next.config.js
@@ -5,11 +5,21 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true
   },
-  images: {
-    formats: ['image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },  
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg | png | jpg | gif | mp4)$/,
+        use: 'file-loader',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
+
+const withVideos = require('next-videos')
+module.exports = withVideos()
