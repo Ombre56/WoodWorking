c96748e9f487641e61ca445416b11269bfc37311
Ombre <krys1014@wp.pl>
2022-04-11 22:02:31 +0200

    Remove dependencies file-loader and next-videos

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg | png | jpg | gif | mp4)$/,
-        use: 'file-loader',
-      },
-    ]
-  },
-}
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+    },
+  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.html$/,
+  //       use: ["html-loader"]
+  //     },
+  //     {
+  //       test: /\.(svg | png | jpg | gif | mp4)$/,
+  //       use: 'file-loader',
+  //     },
+  //   ]
+  // },
 
 module.exports = nextConfig
-
-const withVideos = require('next-videos')
-module.exports = withVideos()
