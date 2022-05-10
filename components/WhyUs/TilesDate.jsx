e3accc337b1ec86f3c306ae00da562c919b145c4
Ombre <krys1014@wp.pl>
2022-05-10 19:51:25 +0200

    Create date to WhyUs section

---
+++ components/WhyUs/TilesDate.jsx
@@ -0,0 +1,26 @@
+export const TilesDate = [
+  {
+    id: 1,
+    title: 'Zaufanie i bezpieczeństwo',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum asperiores, harum beatae in porro praesentium perspiciatis.',
+    icon: '/assets/images/shield.svg'
+  },
+  {
+    id: 2,
+    title: 'Klienci nas kochają',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum asperiores, harum beatae in porro praesentium perspiciatis.',
+    icon: '/assets/images/heart.svg'
+  },
+  {
+    id: 3,
+    title: 'Pomoc online 24/7',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum asperiores, harum beatae in porro praesentium perspiciatis.',
+    icon: '/assets/images/head.svg'
+  },
+  {
+    id: 4,
+    title: '5,000 projektów',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus rerum asperiores, harum beatae in porro praesentium perspiciatis.',
+    icon: '/assets/images/tools.svg'
+  },
+]
