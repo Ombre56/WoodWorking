3091c00b18124b4c11f01f718a1c418a2e34c6dc
Ombre <krys1014@wp.pl>
2022-07-11 18:18:13 +0200

    Add jquery to webpack

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
-    },
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      }),
+    );
+    return config;
   }
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.html$/,
-  //       use: ["html-loader"]
-  //     },
-  //     {
-  //       test: /\.(svg | png | jpg | gif | mp4)$/,
-  //       use: 'file-loader',
-  //     },
-  //   ]
-  // },
+}
 
 module.exports = nextConfig
