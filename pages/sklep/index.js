fda720614f1a5c62b522b31aa819ba2f1ec08fe3
Ombre <krys1014@wp.pl>
2022-12-01 22:14:15 +0100

    Changed index.js in sklep

--- pages/sklep/index.js
+++ pages/sklep/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import Home from '../../sections/Shop/Home';
-import ShopLayout from '../../components/Shop/Home/ShopLayout'
+import Head from 'next/head';
+import { motion } from 'framer-motion';
 
 export default function index() {
-return (
-    <ShopLayout>
-      <Home />
-    </ShopLayout>
+  return (
+  <>
+      <Head>
+        <title>♦ SKLEP STOLARSKI ♦</title>
+        <meta name="description" content="SKlep stolarski" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <motion.div initial="hidden" animate="show">
+        <section id="home">
+            <Home />
+          </section>
+        </motion.div>
+      </main>
+  </>
   );
 };
