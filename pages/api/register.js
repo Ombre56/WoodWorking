27b46814a09ac546cf9ae2cc7b83f559a5fa550c
Ombre <krys1014@wp.pl>
2022-12-12 20:53:37 +0100

    Added api register to registration

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,5 +1,5 @@
-// import bcrypt from 'bcryptjs'
 import Users from '../../model/user'
+
 export default async function handler(req, res) {
 const body = req.body
 const user = await Users.findOne({ email: body.email })
@@ -7,9 +7,7 @@ if (user) {
 res.status(200).json({ message: 'already registered' })
 return;
 }
-// const salt = await bcrypt.genSalt(10);
-// const hashPass = await bcrypt.hash(body.password, salt)
-const newUser = new Users({ email: body.email, password: body.password })
+const newUser = new Users({ name: body.name, surname: body.surname, email: body.email, password: body.password })
 await newUser.save()
 res.status(200).json({ message: 'success' })
 }
