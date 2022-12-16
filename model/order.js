07937072990ae5beb790ee77d8c0b28eef0c50d5
Ombre <krys1014@wp.pl>
2022-12-16 20:07:19 +0100

    Added new model Order

---
+++ model/order.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    customer: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    address: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.models.Order || mongoose.model("Order", OrderSchema);
+
+export default Order;
