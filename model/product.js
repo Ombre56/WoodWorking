b4282cd2ac6d199e8c0a72b92730b54b2f0a3f42
Ombre <krys1014@wp.pl>
2023-03-02 18:14:55 +0100

    Added new field -> category

--- model/product.js
+++ model/product.js
@@ -21,6 +21,10 @@ const productSchema = new Schema({
     type: String,
     required: true
   },
+  category: {
+    type: String,
+    required: true
+  },
   status: {
     type: String,
     required: true
