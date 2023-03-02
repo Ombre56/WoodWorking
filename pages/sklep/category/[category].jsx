224eea97cbc899907b0c79046ffc1c332156fa5c
Ombre <krys1014@wp.pl>
2023-03-02 18:13:56 +0100

    Added a dynamic page with a specific product when you tap on it

---
+++ pages/sklep/category/[category].jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import Head from 'next/head';
+import React from 'react'
+import { motion } from 'framer-motion';
+
+export default function Category({category}) {
+  return (
+    <>
+      <Head>
+        <title>♦ SKLEP STOLARSKI ♦</title>
+        <meta name="description" content="SKlep stolarski" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <motion.div initial="hidden" animate="show">
+          <section id="home">
+            {category}
+          </section>
+        </motion.div>
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const BASE_URL = process.env.BASE_URL;
+
+  const res = await axios.get(
+    `${BASE_URL}/api/products/${params.id}`
+  );
+  return {
+    props: {
+      product: res.data,
+    },
+  };
+};
