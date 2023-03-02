ae630991ea8f9f1de738bdf440c851684c91ddbb
Ombre <krys1014@wp.pl>
2023-03-02 18:20:15 +0100

    Changed message

--- pages/api/register.js
+++ pages/api/register.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 const body = req.body
 const user = await Users.findOne({ email: body.email })
 if (user) {
-res.status(200).json({ message: 'already registered' })
+res.status(200).json({ message: 'Użytkownik o tym adresie już istnieje!' })
 return;
 }
 const newUser = new Users({ name: body.name, surname: body.surname, email: body.email, password: body.password })
